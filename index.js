651d6449d41c9b2f8a97694b37ed1ee27389e56a
Hasib <hasibhsb19@gmail.com>
2026-05-18 02:40:56 +0600

    add mongodb inital setUp and and 10 doctors data in mongoDB than fron backend get all 10 doctors data from mongoDB

--- index.js
+++ index.js
@@ -1,11 +1,52 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 
 const port = process.env.PORT;
+const uri = process.env.MONGODB_URL;
+
+
 app.use(express.json())
 app.use(cors())
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const myDB = client.db("docAppoint");
+    const doctorsCollection = myDB.collection("doctorsdata");
+    // const patientCollention = myDB.collection("patien");
+
+    // Get all doctors data from mongoDB.
+    app.get("/doctors", async (req, res) => {
+        const result = await doctorsCollection.find().toArray();
+        res.send(result);
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
